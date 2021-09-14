36f9032fddd1e81954568345b4af8402f28181b9
최정은 <wish4517@naver.com>
2021-09-14 18:12:21 +0900

    ADD : 유저추천 페이지 시너지 검색

---
+++ frontend/src/detail/recommend/SynergyInfo.js
@@ -0,0 +1,11 @@
+import React from "react"
+function SynergyInfo(props) {
+    const synergy = props.info.rate
+    return (
+      <div>
+        여기에 나중에 정보
+        {synergy}
+      </div>
+    )
+  }
+  export default SynergyInfo;
