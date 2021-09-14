b6580f863baaa0023b553d8702067edf5881a757
민평홍 <vud2009@naver.com>
2021-09-14 17:12:14 +0900

    ADD: DB(MySql) 연결

--- DuoForMe/src/main/java/com/example/DuoForMe/domain/MatchingUser.java
+++ DuoForMe/src/main/java/com/example/DuoForMe/domain/MatchingUser.java
@@ -4,10 +4,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Getter
@@ -16,14 +13,15 @@ import javax.validation.constraints.NotNull;
 public class MatchingUser {
 
     @Id
-    @ManyToOne
+    private Long user_id;
+
+    @MapsId
+    @OneToOne
     @JoinColumn(name = "user_id")
     private User user;
 
-    @NotNull
     private String my_position;
 
-    @NotNull
     private String duo_position;
 
     @Builder
