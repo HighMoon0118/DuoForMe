8c575c6363946091aa3f0b11614f15921a63b697
문학일 <mhi0118@naver.com>
2021-09-15 17:04:05 +0900

    ADD: 로그인, 회원가입, 디테일 페이지

--- frontend/src/detail/Detail.js
+++ frontend/src/detail/Detail.js
@@ -1,11 +1,43 @@
 import React from "react"
-import Recommend from "./recommend/Recommend"
+import { useState } from "react";
+import "./Detail.css"
+import Recommend from "./recommend/Recommend.js"
+import PlayList from "./playlist/PlayList.js"
+import SeasonInfo from "./seasonInfo/SeasonInfo.js"
+import NavBar from "./NavBar.js"
 
 function Detail() {
+
+const [isRecommend, setIsRecommend] = useState(false);
+const [btnMsg, setBtmMsg] = useState("유저 추천")
+
+const toggleRecommend = () => {
+  if (isRecommend) {
+    setIsRecommend(false)
+    setBtmMsg("유저 전적")
+  } else {
+    setIsRecommend(true)
+    setBtmMsg("유저 추천")
+  }
+}
+
   return(
     <div>
-      <div>여기에 위에 있는 부분</div>
-      <Recommend />
+      <NavBar/>
+      <div id="detail">
+        <div id="info">
+          <SeasonInfo/>
+          <button onClick={toggleRecommend}>{ btnMsg }</button>
+          {
+            isRecommend
+            ? <Recommend/>
+            : <PlayList/>
+          }
+        </div>
+        <div id="sidebar">
+          <div><h1>SideBar</h1></div>
+        </div>
+      </div>
     </div>
   )
 }
