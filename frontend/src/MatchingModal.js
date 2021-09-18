d9bccfba07b0e01918d1a2975d78ca84b493e90d
문학일 <mhi0118@naver.com>
2021-09-18 18:58:12 +0900

    ADD: 모달 이동과 폴드 적용

--- frontend/src/MatchingModal.js
+++ frontend/src/MatchingModal.js
@@ -1,24 +1,76 @@
-import React from 'react';
+import React, {useState} from "react"
+import "./MatchingModal.css";
 
-function MatchingModal () {
+const Modal = ( props ) => {
+  // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
+  const { open, close } = props;
+
+
+  const [location, setLocation] = useState({
+    x: 0,
+    y: 50
+  })
+
+  const [startLocation, setStartLocation] = useState({
+    x: 0,
+    y: 0
+  })
+
+  const [mouseLocation, setMouseLocation] = useState({
+    x: 0,
+    y: 0
+  }) 
+
+  const [fold, setFold] = useState(false)
+  const [size, setSize] = useState({w: 700, h:700})
+
+  const dragStart = (e) =>{
+    setMouseLocation({x:e.pageX, y:e.pageY})
+    setStartLocation({x:location.x, y:location.y})
+  }
+  const drag = (e) =>{
+    const xGap = e.pageX - mouseLocation.x
+    const yGap = e.pageY - mouseLocation.y
+    if (Math.abs(location.x-startLocation.x-xGap) <= 50) {
+      setLocation({x:startLocation.x+xGap, y:startLocation.y+yGap})
+    }
+  }
+
+  const foldModal = () => {
+    if (fold) {
+      setSize({w:700, h:700})
+      setFold(false)
+    } else {
+      setSize({w:700, h:60})
+      setFold(true)
+    }
+  }
+
   return (
+    // 모달이 열릴때 openModal 클래스가 생성된다.
     <div>
-      <div id="modal" class="modal-overlay">
-        <div class="modal-window">
-          <div class="title">
-            <h2>모달</h2>
+      {
+        open ? (
+          <div id="matching-modal">
+            <div className="modal" 
+            onDragStart={dragStart}
+            onDrag={drag}
+            style={{left: location.x, top: location.y, height: size.h}}>
+              <div className="header" draggable>
+                매칭 모달
+                <button onClick={ foldModal }>버튼</button>
+              </div>
+              <div className="main" style={fold ? {opacity: 0} : {opacity: 1}}>
+                {props.children}
+              </div>
+              <div className="footer" style={fold ? {opacity: 0} : {opacity: 1}}>
+                <button className="close" onClick={close}> close </button>
+              </div>
+            </div>
           </div>
-          <div class="close-area">X</div>
-          <div class="content">
-            <p>가나다라마바사 아자차카타파하</p>
-            <p>가나다라마바사 아자차카타파하</p>
-            <p>가나다라마바사 아자차카타파하</p>
-            <p>가나다라마바사 아자차카타파하</p>
-          </div>
-        </div>
-      </div>
+        ) : null
+      }
     </div>
-  );
+  )
 }
-
-export default MatchingModal;
+export default Modal
