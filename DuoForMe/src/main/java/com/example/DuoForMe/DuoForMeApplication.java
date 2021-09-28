6be8da8d17a47fea829624005a96db8eb26b6b0d
민평홍 <vud2009@naver.com>
2021-09-28 10:39:03 +0900

    ADD: CORS 설정

--- DuoForMe/src/main/java/com/example/DuoForMe/DuoForMeApplication.java
+++ DuoForMe/src/main/java/com/example/DuoForMe/DuoForMeApplication.java
@@ -10,7 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class DuoForMeApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(DuoForMeApplication.class, args);
 	}
 
