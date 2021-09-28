35749f20209697230717cbadbd17c1d2dfd2976e
최정은 <wish4517@naver.com>
2021-09-28 13:33:45 +0900

    ADD : detail 주소 변경

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <Route path="/" component={ Main } exact={true} />
-      <Route path="/detail" component={ Detail }/>
+      <Route path="/detail/:nickname" component={ Detail }/>
       <Route path="/login" component={ Login }/>
       <Route path="/signup" component={ SignUp }/>
       <Route path="/matching" component={ MatchingModal }/>
