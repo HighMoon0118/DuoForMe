a54b292747c777bdcd09592f13dfbcdfed4bec07
최정은 <wish4517@naver.com>
2021-09-29 00:44:42 +0000

    Merge branch 'feature/matching-redux/fe' into 'develop'
    Feature/matching redux/fe
    
    See merge request s05-bigdata-dist/S05P21B104!22

--- frontend/src/container/MainMatchingContainer.js
+++ frontend/src/container/MainMatchingContainer.js
@@ -2,9 +2,11 @@ import MaingMatching from "../main/MainMatching"
 import { connect } from "react-redux"
 import { changeMatching } from "../modules/matching"
 import { myLine, yourLine } from "../modules/selectLine"
-function MainMatchingContainer ({isMatching, changeMatching, time, me, you, myLine, yourLine}) {
+function MainMatchingContainer ({isMatching, changeMatching, time, me, you, myLine, yourLine, isLogin, history}) {
   return (
-    <MaingMatching isMatching={isMatching} changeMatching={changeMatching} time={Math.floor((new Date().getTime() - new Date(time))/ 1000)} me={me} you={you} myLine={myLine} yourLine={yourLine}/>
+    <MaingMatching isMatching={isMatching} changeMatching={changeMatching} time={Math.floor((new Date().getTime() - new Date(time))/ 1000)} 
+    me={me} you={you} myLine={myLine} yourLine={yourLine} isLogin={isLogin} history={history}
+    />
   )
 }
 function mapStateToProps(state) {
@@ -12,7 +14,8 @@ function mapStateToProps(state) {
     isMatching: state.matching.isMatching,
     time: state.matching.time,
     me: state.selectLine.me,
-    you: state.selectLine.you
+    you: state.selectLine.you,
+    isLogin: state.userInfo.isLogin
   }
 }
 function mapDispatchToProps(dispatch) {
