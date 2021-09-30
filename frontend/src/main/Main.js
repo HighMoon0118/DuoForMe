290a601a642eb50c21685dfa7c731274a0af8ca7
Mincheol Kim <kmc03047@naver.com>
2021-10-01 00:14:04 +0900

    FIX : nohup BE 유지

--- frontend/src/main/Main.js
+++ frontend/src/main/Main.js
@@ -32,7 +32,7 @@ function Main({history, isLogin, logout, isMatching}) {
         
       </div>
       <h1 className="main-font">
-        Duofor.me 왜 안되지?? 3000번 포트 확인
+        Duofor.me nohup을 이용한 배포!
       </h1>
       <MainSearch history={history}/>
       <MainMatchingContainer history={history}/>
