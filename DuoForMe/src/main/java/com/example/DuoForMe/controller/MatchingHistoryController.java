36517e1b5d9faec63fea4864437e9d1223aa21a9
Mincheol Kim <kmc03047@naver.com>
2021-10-05 21:08:10 +0900

    FIX : cross origin 원상복구

--- DuoForMe/src/main/java/com/example/DuoForMe/controller/MatchingHistoryController.java
+++ DuoForMe/src/main/java/com/example/DuoForMe/controller/MatchingHistoryController.java
@@ -18,7 +18,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @Slf4j
-s
+@CrossOrigin("http://localhost:3000/")
 @RequiredArgsConstructor
 @RequestMapping("/api/matchinghistory")
 @RestController
