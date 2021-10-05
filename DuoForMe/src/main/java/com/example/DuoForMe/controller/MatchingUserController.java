e899014d95103378e8bf9c831712166bfeeb1ec2
Mincheol Kim <kmc03047@naver.com>
2021-10-05 22:16:51 +0900

    FIX : cross origin 변경

--- DuoForMe/src/main/java/com/example/DuoForMe/controller/MatchingUserController.java
+++ DuoForMe/src/main/java/com/example/DuoForMe/controller/MatchingUserController.java
@@ -13,7 +13,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @Slf4j
-// @CrossOrigin("http://localhost:3000/")
+@CrossOrigin("http://localhost:3000/")
 @RequiredArgsConstructor
 @RequestMapping("/api/matching")
 @RestController
