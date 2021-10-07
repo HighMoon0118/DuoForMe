50216aac544bcfc7e4ae3ffcf6c6dae6ff703c55
최정은 <wish4517@naver.com>
2021-10-07 20:57:07 +0900

    ADD : css 수정

--- frontend/src/detail/recommend/Recommend.js
+++ frontend/src/detail/recommend/Recommend.js
@@ -1,6 +1,5 @@
 import React from "react"
 import "./Recommend.css"
-import SynergySearch from "./SynergySearch"
 function Recommend({recommend}) {
   // 여기에 나중에 추천 소환사 정보
   const SearchChampions = []
@@ -17,40 +16,42 @@ function Recommend({recommend}) {
   }
   console.log(RecommendUserInfo[0].recomCham)
   const RecommendUsers = RecommendUserInfo.map((user) =>
-    <div key={user.id} className="recommend-info">
-      <div className="top-box">
-        <img src={`https://ddragon.leagueoflegends.com/cdn/11.19.1/img/champion/${user.searchCham}.png`} className="user-image" alt="소환사 이미지"/>
-        <div className="user-info">
-          <div>{user.searchCham}</div>
-        </div>
+    <div className="user-box">
+      <div className="user-info">
+        <div>{user.searchCham}<div style={{display:"inline", marginLeft:"5px"}}>듀오 챔피언 추천 순위</div></div>
       </div>
-      <div className="bottom-box">
-        <div className="champion-box">
-          {user.recomCham[0][0][0] === "null" ? <div className="null"></div> : 
-            <div className="champion-info">
-              <img src={`https://ddragon.leagueoflegends.com/cdn/11.19.1/img/champion/${user.recomCham[0][0][0]}.png`} className="champion-img" alt="챔피언 이미지"/>
-              <div className="champion-name">{user.recomCham[0][0][0]}</div>  
-              <div className="champion-name">{user.recomCham[0][0][1]}</div>  
-            </div>
-          }
+      <div key={user.id} className="recommend-info">
+        <div className="top-box">
+          <img src={`https://ddragon.leagueoflegends.com/cdn/11.19.1/img/champion/${user.searchCham}.png`} className="user-image" alt="소환사 이미지"/>
         </div>
-        <div className="champion-box">
-          {user.recomCham[0][1][0] === "null" ? <div className="null"></div> : 
-            <div className="champion-info">
-              <img src={`https://ddragon.leagueoflegends.com/cdn/11.19.1/img/champion/${user.recomCham[0][1][0]}.png`} className="champion-img" alt="챔피언 이미지"/>
-              <div className="champion-name">{user.recomCham[0][1][0]}</div>  
-              <div className="champion-name">{user.recomCham[0][1][1]}</div>  
-            </div>
-          }
-        </div>
-        <div className="champion-box">
-          {user.recomCham[0][2][0] === "null" ? <div className="null"></div> : 
-            <div className="champion-info">
-              <img src={`https://ddragon.leagueoflegends.com/cdn/11.19.1/img/champion/${user.recomCham[0][2][0]}.png`} className="champion-img" alt="챔피언 이미지"/>
-              <div className="champion-name">{user.recomCham[0][2][0]}</div>  
-              <div className="champion-name">{user.recomCham[0][2][1]}</div>  
-            </div>
-          }
+        <div className="bottom-box">
+          <div className="champion-box">
+            {user.recomCham[0][0][0] === "null" ? <div className="null"></div> : 
+              <div className="champion-info">
+                <img src={`https://ddragon.leagueoflegends.com/cdn/11.19.1/img/champion/${user.recomCham[0][0][0]}.png`} className="champion-img" alt="챔피언 이미지"/>
+                <div className="champion-name">{user.recomCham[0][0][0]}</div>  
+                <div className="champion-name">{user.recomCham[0][0][1]}점</div>  
+              </div>
+            }
+          </div>
+          <div className="champion-box">
+            {user.recomCham[0][1][0] === "null" ? <div className="null"></div> : 
+              <div className="champion-info">
+                <img src={`https://ddragon.leagueoflegends.com/cdn/11.19.1/img/champion/${user.recomCham[0][1][0]}.png`} className="champion-img" alt="챔피언 이미지"/>
+                <div className="champion-name">{user.recomCham[0][1][0]}</div>  
+                <div className="champion-name">{user.recomCham[0][1][1]}점</div>  
+              </div>
+            }
+          </div>
+          <div className="champion-box">
+            {user.recomCham[0][2][0] === "null" ? <div className="null"></div> : 
+              <div className="champion-info">
+                <img src={`https://ddragon.leagueoflegends.com/cdn/11.19.1/img/champion/${user.recomCham[0][2][0]}.png`} className="champion-img" alt="챔피언 이미지"/>
+                <div className="champion-name">{user.recomCham[0][2][0]}</div>  
+                <div className="champion-name">{user.recomCham[0][2][1]}점</div>  
+              </div>
+            }
+          </div>
         </div>
       </div>
     </div>
@@ -61,7 +62,6 @@ function Recommend({recommend}) {
         <div className="recommend-users">
           { RecommendUsers }
         </div>
-        {/* <SynergySearch /> */}
       </div>
     </div>
   )
