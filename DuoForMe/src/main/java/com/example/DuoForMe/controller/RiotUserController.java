46b2260425983404c94ce3521a61e50a765eee97
최정은 <wish4517@naver.com>
2021-10-07 10:31:01 +0900

    FIX : 로컬에서 돌아가게 수정

--- DuoForMe/src/main/java/com/example/DuoForMe/controller/RiotUserController.java
+++ DuoForMe/src/main/java/com/example/DuoForMe/controller/RiotUserController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin("https://duofor.me/")
+@CrossOrigin("http://localhost:3000/")
 @RequestMapping("/api/riotuser")
 public class RiotUserController {
     @Autowired
