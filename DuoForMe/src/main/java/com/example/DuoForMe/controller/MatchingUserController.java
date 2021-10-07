f0c557c3e1b28e99256bd194006f8cf6fe1a3207
Mincheol Kim <kmc03047@naver.com>
2021-10-07 19:50:44 +0900

    FIX : https변경

--- DuoForMe/src/main/java/com/example/DuoForMe/controller/MatchingUserController.java
+++ DuoForMe/src/main/java/com/example/DuoForMe/controller/MatchingUserController.java
@@ -13,7 +13,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @Slf4j
-@CrossOrigin("http://localhost:3000/")
+@CrossOrigin("https://duofor.me/")
 @RequiredArgsConstructor
 @RequestMapping("/api/matching")
 @RestController
